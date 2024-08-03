f46e6a6947078e64dfcae25f09f55473a8d29020
Alex <alexpalex.ah@gmail.com>
2024-08-03 18:08:12 +0300

    created two entity models: Recipee and Ingredient

---
+++ src/main/java/com/recipeshare/project/models/Recipe.java
@@ -0,0 +1,34 @@
+package com.recipeshare.project.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime timeCreated;
+    @UpdateTimestamp
+    private LocalDate timeUpdated;
+}
